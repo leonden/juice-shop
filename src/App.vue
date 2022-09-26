2d8bbc0fc12571d5cf14d98e09334da62ee663a5
hocl1 <leon.hochwimmer@coop.ch>
2022-09-26 16:19:51 +0200

    setup vue app | created first components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <body>
+    <AppNav />
+    <main>
+      <ProductTile />
+    </main>
+  </body>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppNav from "./components/AppNav.vue";
+import ProductTile from "./components/ProductTile.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppNav,
+    ProductTile,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: whitesmoke;
 }
 </style>
---
+++ src/components/ProductDescription.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>description</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDescription",
+};
+</script>
+
+<style lang="scss" scoped></style>
